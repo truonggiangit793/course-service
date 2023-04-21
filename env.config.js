c09d527ab5e43dd54ba387c79ab643f12693ad7c
truonggiangit793 <truonggiangit793@gmail.com>
2023-04-21 14:34:45 +0700

    Update code

--- env.config.js
+++ env.config.js
@@ -5,6 +5,7 @@ module.exports = {
     SALT: 2023,
     PORT: process.env.PORT,
     DB_URL: process.env.DB_URL,
+    ClIENT_SERVICE: process.env.ClIENT_SERVICE,
     BASE_URL: `http://${ip.address()}:${process.env.PORT || 3000}`,
     SERVICE_KEY: ["wGlWnNDcrU0K4zOF8ywGl", "iCkXIeYOYsIOrGGla92Sr", "WnNDcrU0okPhTVA27BLe3IJ8y"],
 };
