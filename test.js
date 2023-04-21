8bb1f49b1a895aff0e4d338b9decdbad5cb20733
truonggiangit793 <truonggiangit793@gmail.com>
2023-04-21 15:52:39 +0700

    Update code

---
+++ test.js
@@ -0,0 +1,11 @@
+const data = {
+    courseCode: 2023004,
+    semesterAlias: "HK1-2023-2024",
+    classId: 1,
+    groupId: 1,
+    limit: 30,
+    studentMember: [123, 456, 789],
+    periods: [1, 2, 3],
+    weeks: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 15],
+    days: "Wednesday",
+};
